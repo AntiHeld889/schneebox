111bb45b104ce69931cbea99ad3228617f5666ef
Reik Braune <Reik.Braune@me.com>
2024-05-31 10:16:27 +0200

    Update main.py

--- V1/main.py
+++ V1/main.py
@@ -6,16 +6,16 @@ import json
 
 """Your main code goes here!"""
 
-print("V5")
+print("V4")
 print('GPRS-Status:', cellular.gprs())
+time.sleep(4)
 
 try:
-  if not cellular.gprs():
-    try:
-      print("Trying to connect\r\n")
-      cellular.gprs("pepper", "", "")
-      print("Connected to gprs...\r\n")
-    except Exception as e:
-        print("Couldn't connect to gprs:", e)
-        machine.reset()
+    print("Trying to connect\r\n")
+    cellular.gprs("pepper", "", "")
+    print("Connected to gprs...\r\n")
+except Exception as e:
+    print("Couldn't connect to gprs:", e)
+    machine.reset()
+
 
